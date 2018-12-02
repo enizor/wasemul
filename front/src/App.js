7412799440c10d397f072fe53fdbc61fc2abbaf9
Kylian Serrania <kylian.serrania@student.ecp.fr>
2018-12-03 00:52:26 +0100

    Docker(Front): Revert to local machine npm package use in dev environment

--- front/src/App.js
+++ front/src/App.js
@@ -7,11 +7,16 @@ const App = () => (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit aaa
-        <code>src/App.js</code>
+        Edit
+        <code> src/App.js </code>
         and save to reload.
       </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Learn React
       </a>
     </header>
