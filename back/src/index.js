d4e28fc7cc8dad801daca120ad1ec36fc223449e
Kylian Serrania <kylian.serrania@student.ecp.fr>
2018-12-02 16:38:53 +0100

    Feat(Back): Add sample express server

---
+++ back/src/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+
+import express from 'express';
+
+const app = express();
+const port = 3001;
+
+app.get('/', (_, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
