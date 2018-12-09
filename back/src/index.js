128d7ff400dd3700d0cf0b1e3c94542bf7d806b6
Kylian Serrania <kylian.serrania@student.ecp.fr>
2018-12-09 23:02:01 +0100

    Feat(Back): Add middlewares

--- back/src/index.js
+++ back/src/index.js
@@ -1,10 +1,32 @@
 /* eslint-disable no-console */
 
 import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import errorHandler from 'errorhandler';
+import morgan from 'morgan';
 
 const app = express();
 const port = 3001;
 
-app.get('/', (_, res) => res.send('Hello World!'));
+// Parse application/json
+app.use(bodyParser.json());
+
+dotenv.load();
+
+sequelize.sync();
+
+if (process.env.NODE_ENV === 'development') {
+  // Show better errors in development
+  app.use(errorHandler());
+
+  // Log http accesses
+  app.use(morgan('dev'));
+
+}
+
+app.get('/', (_, res) => {
+  res.send('Hello world!');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
