c7b1a7e4462dabaafd35f4fa881ab86965ac18ec
Djou <djou@via.ecp.fr>
2018-12-17 16:54:58 +0100

    Fix(all): merge for cors

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import User from './components/User';
+import Game from './components/Game';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/users/:id" component={User} />
+          <Route path="/games/:id" component={Game} />
           {/* default route */}
           <Route component={Home} />
         </Switch>
