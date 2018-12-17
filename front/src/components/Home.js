282ab56ef17d5fc8abcefdbc737d2cdf55b70300
lithium <louis.cabrol@student.ecp.fr>
2018-12-17 15:40:33 +0100

    fix(): eslint magic

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="App pure-g center">
-        <h1 className="pure-u-3-5 text-left">Je suis le home</h1>
-      </div>
-    );
-  }
+function Home() {
+  return (
+    <div className="App pure-g center">
+      <h1 className="pure-u-3-5 text-left">Je suis le home</h1>
+    </div>
+  );
 }
 
 export default Home;
--- front/src/components/User.js
+++ front/src/components/User.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class User extends React.Component {
   constructor(props) {
@@ -9,21 +10,59 @@ class User extends React.Component {
   }
 
   componentDidMount() {
-    fetch(`http://localhost:3001/users/${this.props.match.params.id}`)
+    const { match } = this.props;
+    fetch(`http://localhost:3001/users/${match.params.id}`)
       .then(res => res.json())
       .then((result) => {
         this.setState({ user: result });
       });
   }
 
+  //   {
+  //     "id": 1,
+  //     "nickname": "test",
+  //     "email": "test@example.com",
+  //     "authLevel": 0,
+  //     "biography": null,
+  //     "icon": null,
+  //     "enabled": null,
+  //     "createdAt": "2018-12-10T15:03:34.773Z",
+  //     "updatedAt": "2018-12-10T15:03:34.773Z"
+  //     }
   render() {
+    const { user } = this.state;
     return (
       <div className="App pure-g center">
-        <h1 className="pure-u-3-5 text-left">OWO</h1>
-        <p>{this.state.user.nickname}</p>
+        <h1 className="pure-u-3-5 text-left">Utilisateur</h1>
+        <p>
+          Nickname:
+          {user.nickname}
+        </p>
+        <p>
+          Email:
+          {user.email}
+        </p>
+        {user.biography != null && (
+          <p>
+            Biography:
+            {user.biography}
+          </p>
+        )}
       </div>
     );
   }
 }
+
+User.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  }),
+};
+
+User.defaultProps = {
+  match: { params: { id: 0 } },
+};
 
 export default User;
