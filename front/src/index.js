c11770d8ba97918922a3aa0e7b4cc90ff11965bb
lithium <louis.cabrol@student.ecp.fr>
2018-12-17 15:17:36 +0100

    Feat: skeleton code for user

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
