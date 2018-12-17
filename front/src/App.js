9252bb4e3c956072b2d2900ed7c5d8a92530a379
Kylian Serrania <kylian.serrania@student.ecp.fr>
2018-12-17 14:22:07 +0100

    Config(Front): Put components in components folder

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import User from './components/User';
--- front/src/components/Game.js
+++ front/src/components/Game.js
@@ -2,20 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Game = (props) => {
-  const {
-    id, name, publisher, releaseDate, icon,
-  } = props;
-  const iconPath = `http://localhost:3001/static/${icon}`;
+  const { id, name } = props;
   return (
     <a className="pure-button pure-u-3-5" href={`/games/${id}`}>
       <div className="Game">
-        <div className="pure-u-1-5">
-          <img src={iconPath} height="128" alt={`${name}`} />
-        </div>
-        <div className="pure-u-4-5">
-          <h3>{`${id}: ${name}`}</h3>
-          <h4>{`${publisher} (${releaseDate})`}</h4>
-        </div>
+        <h3>{`${id}: ${name}`}</h3>
       </div>
     </a>
   );
@@ -24,9 +15,6 @@ const Game = (props) => {
 Game.propTypes = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
-  publisher: PropTypes.string.isRequired,
-  releaseDate: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
 };
 
 export default Game;
