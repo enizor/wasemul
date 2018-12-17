ee71973b1d8c1a8a728438aa98332540fe0f29a0
Rémi Garde <remi.garde@free.fr>
2018-12-17 16:25:32 +0100

    Feat(front:games): add pure-menu CSS

--- front/src/components/GameList.js
+++ front/src/components/GameList.js
@@ -21,18 +21,22 @@ class GameList extends React.Component {
 
     return (
 
-      <div>
-        {games.map(game => (
-          <a
-            key={game.id}
-            className="pure-button pure-u-3-5"
-            href={`/games/${game.id}`}
-          >
-            <div className="Game">
-              <h3>{`${game.id}: ${game.name}`}</h3>
-            </div>
-          </a>
-        ))}
+      <div className="pure-menu" style={{ display: 'inline-block' }}>
+        <span className="pure-menu-heading">Featured Games</span>
+        <ul className="pure-menu-list">
+          {games.map(game => (
+            <li key={game.id} className="pure-menu-item">
+              <a
+                className="pure-menu-link"
+                href={`/games/${game.id}`}
+              >
+                <div className="Game">
+                  <h3>{`${game.id}: ${game.name}`}</h3>
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
