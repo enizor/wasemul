b3fcb75a602a1297a90090f4880f7df1466e3cfe
Kylian Serrania <kylian.serrania@student.ecp.fr>
2018-12-17 14:22:07 +0100

    Config(Front): Put components in components folder

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import User from './components/User';
+import Game from './components/Game';
+import './App.css';
 
 export default function App() {
   return (
