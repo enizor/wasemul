5678dc2ab957d4539409d3cd73f816c3fb984e59
Kylian Serrania <kylian.serrania@student.ecp.fr>
2018-12-17 16:46:01 +0100

    Chore(*): Clean up merge problems

--- front/src/App.js
+++ front/src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import User from './components/User';
 
-
 export default function App() {
   return (
     <Router>
@@ -20,4 +19,3 @@ export default function App() {
     </Router>
   );
 }
-
