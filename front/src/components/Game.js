6dd99165fd1081450070bdc92de5b25ae7282f49
Kylian Serrania <kylian.serrania@student.ecp.fr>
2018-12-17 15:49:27 +0100

    Feat(Front/Back): Add icons

--- front/src/components/Game.js
+++ front/src/components/Game.js
@@ -2,19 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Game = (props) => {
-  const { id, name, publisher, releaseDate } = props;
+  const {
+    id, name, publisher, releaseDate, icon,
+  } = props;
+  const iconPath = `http://localhost:3001/static/${icon}`;
   return (
     <a className="pure-button pure-u-3-5" href={`/games/${id}`}>
       <div className="Game">
         <div className="pure-u-1-5">
-          <img src="https://people.via.ecp.fr/~atauveron/img/logo_VIA.jpg" height="128" alt="VIA Centrale Réseaux" />
+          <img src={iconPath} height="128" alt="VIA Centrale Réseaux" />
         </div>
         <div className="pure-u-4-5">
           <h3>{`${id}: ${name}`}</h3>
           <h4>{`${publisher} (${releaseDate})`}</h4>
         </div>
       </div>
-    </a >
+    </a>
   );
 };
 
@@ -23,6 +26,7 @@ Game.propTypes = {
   name: PropTypes.string.isRequired,
   publisher: PropTypes.string.isRequired,
   releaseDate: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
 };
 
 export default Game;
