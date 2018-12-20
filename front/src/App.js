4bf84661929b2d43c4b5a5dbcde3cbfbf9ef84e1
Aimery Tauveron <atauveron@via.ecp.fr>
2018-12-20 11:22:52 +0100

    Feat(Front): add basic NavBar

--- front/src/App.js
+++ front/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import User from './components/User';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <Router>
       <div>
+        <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/users/:id" component={User} />
