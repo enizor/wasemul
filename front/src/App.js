2d42d9d9354c1f18d875a62bd0f6169a18631859
Aimery Tauveron <atauveron@via.ecp.fr>
2018-12-21 15:39:35 +0100

    Fix(Front): fix front

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
 import User from './components/User';
-import Game from './components/Game';
 import Navbar from './components/Navbar';
 import './App.css';
 
