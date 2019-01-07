62ed350568ecc08829b6888398c4ff13792600db
Djou <djou@via.ecp.fr>
2019-01-07 16:33:04 +0100

    Feat(NavBar): Replaced old navbar by a shiny barand new react boostrap navbar

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
 import SearchBar from './SearchBar';
 
-import '../css/Navbar.css';
+const NavigationBar = () => (
+  <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/">React-Bootstrap</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="/games">
+          PokemonGreenGame
+        </NavItem>
+        <NavItem eventKey={2} href="/users/3">
+          AdminUser
+        </NavItem>
+      </Nav>
+      <Nav pullRight>
+        <NavItem>
+          <SearchBar />
+        </NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
 
-function Navbar() {
-  // Loosely based on https://codepen.io/jo_Geek/pen/xgbaEr for HTML and CSS code
-  return (
-    <div className="nav">
-      <div className="nav-header">
-        <div className="nav-title">Wasemul</div>
-      </div>
-      <input type="checkbox" id="nav-check" />
-      <div className="nav-links">
-        <a href="/">Home</a>
-        <a href="/users/1">First user</a>
-        <SearchBar />
-      </div>
-    </div>
-  );
-}
-
-export default Navbar;
+export default NavigationBar;
