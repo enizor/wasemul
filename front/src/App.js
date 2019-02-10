71fda16c2a6e395c9ff1d5fcca00521935f85910
Kylian Serrania <kylian.serrania@student.ecp.fr>
2019-02-10 17:46:25 +0100

    Feat(Front): Add EditUser route

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
 import User from './components/User';
+import EditUser from './components/EditUser';
 import Game from './components/Game';
 import Navbar from './components/Navbar';
 import SearchPage from './components/SearchPage';
@@ -15,6 +16,7 @@ export default function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/users/:id/edit" component={EditUser} />
           <Route path="/users/:id" component={User} />
           <Route path="/games/:id" component={Game} />
           <Route path="/search/:query" component={SearchPage} />
