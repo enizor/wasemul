2737d67c10fe05249affba845d53f83dae9ba488
Kylian Serrania <kylian.serrania@student.ecp.fr>
2019-02-17 22:00:19 +0100

    Fix(Front): Use correct CSS for GameItem

--- front/src/components/GameItem.js
+++ front/src/components/GameItem.js
@@ -5,10 +5,12 @@ const GameItem = (props) => {
   const {
     id, name, publisher, releaseDate, icon,
   } = props;
+
   const iconPath = icon;
+
   return (
     <a className="pure-button pure-u-4-5" href={`/games/${id}`}>
-      <div className="Game">
+      <div className="GameItem">
         <div className="pure-u-1-5">
           <img src={iconPath} height="128" alt={name} />
         </div>
