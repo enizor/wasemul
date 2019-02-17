0213c15c7b91a14f51df876a1c02d256753792bf
lithium <louis.cabrol@student.ecp.fr>
2019-02-17 14:25:13 +0100

    fix: cleaner async handling in game.js

--- back/src/controllers/game.js
+++ back/src/controllers/game.js
@@ -64,15 +64,14 @@ const findFeaturedGames = async (_, res) => {
     comments.push(games[i].getComments({}));
   }
   const minTime = Math.min(...times);
-  Promise.all(comments).then((result) => {
-    for (let i = 0; i < games.length; i += 1) {
-      games[i].score = 0;
-      games[i].score += (times[i] - minTime) / 1000;
-      games[i].score += result[i].length;
-    }
-    games.sort((a, b) => b.score - a.score);
-    res.send(games);
-  });
+  const result = await Promise.all(comments);
+  for (let i = 0; i < games.length; i += 1) {
+    games[i].score = 0;
+    games[i].score += (times[i] - minTime) / 1000;
+    games[i].score += result[i].length;
+  }
+  games.sort((a, b) => b.score - a.score);
+  res.send(games);
 };
 
 const createGame = async (req, res) => {
